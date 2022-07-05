e96f048625910b171524130c2f15a8fa39ccf0fe
SaravananKS23 <91030871+SaravananKS23@users.noreply.github.com>
2022-07-05 17:15:31 +0530

    Add files via upload

---
+++ src/components/array.js
@@ -0,0 +1,31 @@
+// Javascript object named array with 3 key-values
+const array = [
+    {
+      id:'1',
+      Name:'Ajith Kumar',
+      Age:'23'
+    },
+    {
+      id:'2',
+      Name:'Ramesh',
+      Age:'22'
+    },
+    {
+      id:'3',
+      Name:'Ravi',
+      Age:'25'
+    },
+    {
+        id:'4',
+        Name:'Jeyanth',
+        Age:'28'
+      },
+      {
+        id:'5',
+        Name:'Ganesh',
+        Age:'32'
+      }
+      ]
+  
+  export default array
+  
